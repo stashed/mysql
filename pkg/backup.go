4b2e0e791fee1807e1081634129c09862266704a
piyush1146115 <piyush@appscode.com>
2022-06-03 17:47:56 +0600

    Refactor implementation.
    Signed-off-by: piyush1146115 <piyush@appscode.com>

--- pkg/backup.go
+++ pkg/backup.go
@@ -30,7 +30,6 @@ import (
 	"gomodules.xyz/flags"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	appcatalog "kmodules.xyz/custom-resources/apis/appcatalog/v1alpha1"
 	appcatalog_cs "kmodules.xyz/custom-resources/client/clientset/versioned"
@@ -70,6 +69,8 @@ func NewCmdBackup() *cobra.Command {
 			if err != nil {
 				return err
 			}
+			opt.config = config
+
 			opt.kubeClient, err = kubernetes.NewForConfig(config)
 			if err != nil {
 				return err
@@ -89,7 +90,7 @@ func NewCmdBackup() *cobra.Command {
 				Namespace:  opt.appBindingNamespace,
 			}
 			var backupOutput *restic.BackupOutput
-			backupOutput, err = opt.backupMySQL(targetRef, config)
+			backupOutput, err = opt.backupMySQL(targetRef)
 			if err != nil {
 				backupOutput = &restic.BackupOutput{
 					BackupTargetStatus: api_v1beta1.BackupTargetStatus{
@@ -150,9 +151,9 @@ func NewCmdBackup() *cobra.Command {
 	return cmd
 }
 
-func (opt *mysqlOptions) backupMySQL(targetRef api_v1beta1.TargetRef, config *restclient.Config) (*restic.BackupOutput, error) {
+func (opt *mysqlOptions) backupMySQL(targetRef api_v1beta1.TargetRef) (*restic.BackupOutput, error) {
 	var err error
-	err = license.CheckLicenseEndpoint(config, licenseApiService, SupportedProducts)
+	err = license.CheckLicenseEndpoint(opt.config, licenseApiService, SupportedProducts)
 	if err != nil {
 		return nil, err
 	}
